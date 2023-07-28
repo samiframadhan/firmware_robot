1215f025c382692b8e6e8a420d9413ea734a391b
Sami Fauzan Ramadhan <sfauzanramadhan@gmail.com>
2023-07-28 20:29:29 +0700

    [Fix] Log level correction

--- lib/Motor/src/Motor.cpp
+++ lib/Motor/src/Motor.cpp
@@ -20,7 +20,7 @@ void Motor::config(motor_configs conf){
     motor_encoder.attach_single_edge(configs.pin_encoder, configs.pin_direction);
     pinMode(configs.pin_direction, OUTPUT);
     pinMode(configs.pin_enable, OUTPUT);
-    // ESP_LOGE(TAG, "Pin Direction: %d, Pin Enable: %d", configs.pin_direction, configs.pin_enable);
+    // ESP_LOGI(TAG, "Pin Direction: %d, Pin Enable: %d", configs.pin_direction, configs.pin_enable);
     set_pinpwm(configs.pin_pwm);
     if(configs.pin_pwm != 0){
         ledcAttachPin(configs.pin_pwm, this->pwm_channel);
@@ -49,29 +49,29 @@ bool Motor::set_pwm(int pwm){
 
     if(pwm > 0){
         ledcWrite(this->pwm_channel, pwm);
-        // ESP_LOGE(TAG, "PWM Channel invoked: %d, Pin Enable: %d is %d", this->pwm_channel, configs.pin_enable, 1);
-        ESP_LOGE(TAG, "PWM Val: %d", pwm);
+        // ESP_LOGI(TAG, "PWM Channel invoked: %d, Pin Enable: %d is %d", this->pwm_channel, configs.pin_enable, 1);
+        ESP_LOGI(TAG, "PWM Val: %d", pwm);
         if(configs.reversed){
             digitalWrite(configs.pin_direction, HIGH);
-            ESP_LOGE(TAG, "Direction: should be 1 currently %d", digitalRead(configs.pin_direction));
-            // ESP_LOGE(TAG, "Pin Direction: %d is %d", configs.pin_direction, BACKWARD);
+            ESP_LOGI(TAG, "Direction: should be 1 currently %d", digitalRead(configs.pin_direction));
+            // ESP_LOGI(TAG, "Pin Direction: %d is %d", configs.pin_direction, BACKWARD);
         } else {
             digitalWrite(configs.pin_direction, LOW);
-            ESP_LOGE(TAG, "Direction: should be 0 currently %d", digitalRead(configs.pin_direction));
-            // ESP_LOGE(TAG, "Pin Direction: %d is %d", configs.pin_direction, FORWARD);
+            ESP_LOGI(TAG, "Direction: should be 0 currently %d", digitalRead(configs.pin_direction));
+            // ESP_LOGI(TAG, "Pin Direction: %d is %d", configs.pin_direction, FORWARD);
         }
         return 1;
     } else {
         ledcWrite(this->pwm_channel, abs(pwm));
-        ESP_LOGE(TAG, "PWM Val: %d", pwm);
+        ESP_LOGI(TAG, "PWM Val: %d", pwm);
         if(configs.reversed){
             digitalWrite(configs.pin_direction, LOW);
-            ESP_LOGE(TAG, "Direction: should be 0 currently %d", digitalRead(configs.pin_direction));
-            // ESP_LOGE(TAG, "Pin Direction: %d is %d", configs.pin_direction, FORWARD);
+            ESP_LOGI(TAG, "Direction: should be 0 currently %d", digitalRead(configs.pin_direction));
+            // ESP_LOGI(TAG, "Pin Direction: %d is %d", configs.pin_direction, FORWARD);
         } else {
             digitalWrite(configs.pin_direction, HIGH);
-            ESP_LOGE(TAG, "Direction: should be 1 currently %d", digitalRead(configs.pin_direction));
-            // ESP_LOGE(TAG, "Pin Direction: %d is %d", configs.pin_direction, BACKWARD);
+            ESP_LOGI(TAG, "Direction: should be 1 currently %d", digitalRead(configs.pin_direction));
+            // ESP_LOGI(TAG, "Pin Direction: %d is %d", configs.pin_direction, BACKWARD);
         }
         return 1;
     }
