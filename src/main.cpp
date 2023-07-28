b99a5a213af092fa42c0eac16fe8871ae4e07e29
Sami Fauzan Ramadhan <sfauzanramadhan@gmail.com>
2023-07-28 19:00:32 +0700

    [Update] try protobuf

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 Motor motor_kiri;
 Motor motor_kanan;
 int count = 200;
+uint8_t buffer[128];
+size_t message_length;
+bool status;
 
 static char* TAG = "Debugging";
 
@@ -23,20 +26,38 @@ void setup() {
   left_motor.reversed       = true;
   left_motor.ppr            = 10;
 
-  right_motor.pin_direction = 19;
-  right_motor.pin_enable    = 14;
-  right_motor.pin_pwm       = 18;
-  right_motor.pin_encoder   = 15;
+  right_motor.pin_direction = 19;   // VR
+  right_motor.pin_enable    = 14;   // EN/EL
+  right_motor.pin_pwm       = 18;   // Z/F
+  right_motor.pin_encoder   = 15;   // Signal
   right_motor.pwm_freq      = 1000;
   right_motor.reversed      = true;
   right_motor.ppr           = 10;
 
+  motorData message = motorData_init_zero;
+
+  pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+  message.direction = true;
+  message.speed = 1000;
+
+  status = pb_encode(&stream, motorData_fields, &message);
+  message_length = stream.bytes_written;
+  
   motor_kiri.config(left_motor);
   motor_kanan.config(right_motor);
   // motor_kanan.set_pindir(15, true);
   // motor_kanan.set_pinpwm(13);
   // motor_kanan.set_enable(12);
+
+  motorData motormsg = motorData_init_zero;
+  pb_istream_t s = pb_istream_from_buffer(buffer, message_length);
+  status = pb_decode(&s, motorData_fields, &motormsg);
+
   Serial.begin(115200);
+  if(status){
+    Serial.print("Berhasil!");
+    
+  }
 }
 
 void loop() {
